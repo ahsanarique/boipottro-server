bc595be0bec553ba7b80b100d99863ab9a44aa4f
ahsanarique <ahsan.arique@gmail.com>
2021-04-01 03:20:36 +0600

    mongo connect

---
+++ README.md
@@ -0,0 +1,3 @@
+# Boipottro Server
+
+## Api: TBA
--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
-// const ObjectID = require("mongodb").ObjectID;
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const userName = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
+const dbCollection = process.env.DB_COLLECTION;
+
+const uri = `mongodb+srv://${userName}:${pass}@cluster0.twaro.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+client.connect((err) => {
+  const collection = client.db(dbName).collection(dbCollection);
+
+  if (err) {
+    console.log("Database connection failed", err);
+  } else {
+    console.log("Successfully connected to database");
+  }
+});
 
 const port = process.env.PORT || 5000;
 
